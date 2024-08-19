38a46671e386d012b373bedf0a14ab8ec492259f
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-19 12:41:37 +0400

    Optimize: Handle Mongodb Connection

--- index.js
+++ index.js
@@ -12,13 +12,35 @@ app.use(express.json());
 app.use(cors());
 app.use(requestIp.mw());
 
+let isConnected;
+
 mongoose
-  .connect(process.env.MONGODB_URI, {})
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    isConnected = true;
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    isConnected = false;
+  });
+
+async function ensureMongoDBConnection() {
+  if (!isConnected) {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+  }
+}
 
 app.get("/", async (req, res) => {
   try {
+    await ensureMongoDBConnection();
     const ipAddress = await Ip.find();
     res.setHeader("Content-Type", "application/json");
     res.json(ipAddress);
@@ -30,6 +52,7 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
+    await ensureMongoDBConnection();
     const clientIp = req.clientIp;
     console.log(clientIp, "client ip");
 
@@ -40,8 +63,4 @@ app.post("/", async (req, res) => {
     console.error("Error saving IP:", error);
     res.sendStatus(500);
   }
-});
-
-app.listen(PORT, () => {
-  console.log("Server is listening on port", PORT);
 });
